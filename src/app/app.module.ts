7daf81f70bea1af72288037790f0a43b93342f1a
Rotha-Vichet <sydianicmare@gmail.com>
2022-02-07 19:25:32 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BackgroundImageComponent } from './background-image/background-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundImageComponent
   ],
   imports: [
     BrowserModule
